00539147311d1a544583671eed13e5eb791351b7
josip samardzic <josip.samardzic2@gmail.com>
2023-11-18 00:21:57 +0100

    scanf

--- rush00.c
+++ rush00.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
 
+int main()
+{
+	char row;
+	char col;
+
+	printf("Enter row number: \n");
+	scanf("%s", &row);
+	printf("Enter col number: \n");
+	scanf("%s", &col);
+	return (0);
+}
