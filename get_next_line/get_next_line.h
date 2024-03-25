ba9e8f1af57d7cfc26a3269a17b7f124380937eb
Josip Samardzic <jsamardz@student.42heilbronn.de>
2024-03-25 12:56:02 +0100

    get_next_line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsamardz <jsamardz@student.42heilnronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/25 11:50:19 by jsamardz          #+#    #+#             */
+/*   Updated: 2024/03/25 11:55:55 by jsamardz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+char *get_next_line(int fd);
+
+#endif
