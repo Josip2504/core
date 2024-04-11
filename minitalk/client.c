62d485965032421e786acf3a2cd92af694719fa0
Josip Samardzic <jsamardz@student.42heilbronn.de>
2024-04-11 12:07:05 +0200

    s

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,24 @@
 /*   By: jsamardz <jsamardz@student.42heilnronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 10:51:00 by jsamardz          #+#    #+#             */
-/*   Updated: 2024/04/11 10:51:01 by jsamardz         ###   ########.fr       */
+/*   Updated: 2024/04/11 12:04:49 by jsamardz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+int	main(int ac, char **av)
+{
+	pid_t	server_pid;
+	char	*str;
+
+	str = av[2];
+	server_pid = ft_atoi(av[1]);
+	if (ac == 3)
+	{
+
+	}
+	else
+		return (-1);
+	return (0);
+}
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,16 @@
 /*   By: jsamardz <jsamardz@student.42heilnronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 10:51:11 by jsamardz          #+#    #+#             */
-/*   Updated: 2024/04/11 10:51:12 by jsamardz         ###   ########.fr       */
+/*   Updated: 2024/04/11 12:03:21 by jsamardz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+int	main(void)
+{
+	pid_t server_pid;
+
+	server_pid = getpid();
+	ft_printf("Server PID: %d\n", server_pid);
+}
