09f6b3fa989d0ba85dbc8e06b0a62f2ed7aa782b
Josip Samardzic <jsamardz@student.42heilbronn.de>
2024-04-12 10:40:19 +0200

    sending signals

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,54 +6,24 @@
 /*   By: jsamardz <jsamardz@student.42heilnronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 10:51:00 by jsamardz          #+#    #+#             */
-/*   Updated: 2024/04/11 16:25:44 by jsamardz         ###   ########.fr       */
+/*   Updated: 2024/04/12 10:06:12 by jsamardz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-// void	ft_bits(char *str, char *bits)
-// {
-// 	int	i;
-// 	int	j;
-
-// 	i = 0;
-// 	while (str[i])
-// 	{
-// 		j = 7;
-// 		while (j >= 0)
-// 		{
-// 			bits[(i * 8) + (7 - j)] = ((str[i] >> j) & 1) + '0';
-// 			j--;
-// 		}
-// 		i++;
-// 	}
-// 	bits[i * 8] = '\0';
-// }
-
-void	ft_send(pid_t server_pid, char *str)
+void	ft_send(pid_t server_pid, char letter)
 {
 	int	i;
 
 	i = 0;
-	while (str[i])
+	while (i < 8)
 	{
-		if (str[i] == '1')
-		{
-			kill(server_pid, SIGUSR1);
-			exit(EXIT_SUCCESS);
-		}
-		else if (str[i] == '2')
-		{
+		if ((letter & (0x01 << i)) == 0)
 			kill(server_pid, SIGUSR2);
-			exit(EXIT_SUCCESS);
-		}
 		else
-		{
-			ft_printf("invalid char");
-			exit(EXIT_FAILURE);
-		}
-		usleep(500000);
+			kill(server_pid, SIGUSR1);
+		i++;
 	}
 }
 
@@ -61,16 +31,19 @@ int	main(int ac, char **av)
 {
 	pid_t	server_pid;
 	char	*str;
-	// char	bits[8 * ft_strlen(str) + 1];
+	int		i;
 
+	i = 0;
 	str = av[2];
 	server_pid = ft_atoi(av[1]);
 	if (ac == 3)
 	{
-		// ft_bits(str, bits);
-		// ft_printf("%s\n", av[2]);
-		// ft_printf("%s", bits);
-		ft_send(server_pid, str);
+		while (*str)
+		{
+			ft_send(server_pid, *str);
+			str++;
+		}
+		ft_send(server_pid, '\n');
 	}
 	else
 		return (-1);
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: jsamardz <jsamardz@student.42heilnronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 10:51:11 by jsamardz          #+#    #+#             */
-/*   Updated: 2024/04/11 16:10:12 by jsamardz         ###   ########.fr       */
+/*   Updated: 2024/04/12 10:36:34 by jsamardz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,32 @@
 
 void	signal_handler(int signum)
 {
-	if (signum == SIGUSR1)
-		ft_printf("receved s1\n");
-	else if (signum == SIGUSR2)
-		ft_printf("receved s2\n");
+	static int	str;
+	static int	bitcount;
+
+	if (signum == SIGUSR2)
+		str |= (0x01 << bitcount);
+	bitcount++;
+	if (bitcount == 8)
+	{
+		if (str == '\n')
+			ft_printf("end\n");
+		else
+			ft_printf("message: %c\n", str);
+		str = 0;
+		bitcount = 0;
+	}
 }
 
 int	main(void)
 {
-	pid_t server_pid;
+	pid_t	server_pid;
 
 	server_pid = getpid();
 	ft_printf("Server PID: %d\n", server_pid);
 	signal(SIGUSR1, signal_handler);
 	signal(SIGUSR2, signal_handler);
 	while (1)
-		sleep(1);
+		pause();
 	return (0);
 }
