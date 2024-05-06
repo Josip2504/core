35e0e5627a3a7321a7a49b1175645cd24a25610a
Josip Samardzic <jsamardz@student.42heilbronn.de>
2024-05-06 14:25:50 +0200

    added to stack

--- push_swap/init.c
+++ push_swap/init.c
@@ -1,32 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jsamardz <jsamardz@student.42heilnronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/30 10:50:03 by jsamardz          #+#    #+#             */
-/*   Updated: 2024/05/06 14:24:11 by jsamardz         ###   ########.fr       */
+/*   Created: 2024/05/06 12:47:02 by jsamardz          #+#    #+#             */
+/*   Updated: 2024/05/06 14:24:23 by jsamardz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int ac, char **av)
-{
-	t_stack *stack_a;
-	t_stack *stack_b;
-	int		size_stack;
+//filling stack
 
-	stack_b = NULL;
-	if (ac < 2)
-		return (-1);
-	ft_input(ac, av);
-	stack_a = fill_stack(ac, av);
-	size_stack = stack_size(stack_a);
-	ft_printf("%d", size_stack);
-	// check is sorted, if is -> free stacks
-	// sort stacks
-	// free stacks
-	return (0);
+t_stack	*fill_stack(int ac, char **av)
+{
+	t_stack	*stack_a;
+	int		i;
+	int		num;
+
+	num = 0;
+	i = 1;
+	stack_a = NULL;
+	while (i < ac)
+	{
+		num = ft_atoi(av[i]);
+		if (i == 1)
+			stack_a = new_stack(num);
+		else
+			add_to_bottom(new_stack(num), &stack_a);
+		i++;
+	}
+	return (stack_a);
 }
